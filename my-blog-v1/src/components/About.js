f6ebff1b5fe6f13c895c270e8ff67534835c529c
Lauren <rg326@live.com>
2021-11-21 18:01:32 -0500

    edit about comp

--- my-blog-v1/src/components/About.js
+++ my-blog-v1/src/components/About.js
@@ -1,8 +1,16 @@
-function About (){
+import React from 'react';
+
+function AboutMe(props) {
     return (
-    
-    
-    )
+        <div>
+           <h1>About Me</h1> 
+           <section>
+               <p>
+                   
+               </p>
+           </section>
+        </div>
+        )
 }
 
-export default About;
+export default AboutMe;
