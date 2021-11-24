258d76a3883f2dc0b3b55b185215031474990a69
Lauren <rg326@live.com>
2021-11-23 21:58:50 -0500

    added server

--- website-layouts/cms-build/app.js
+++ website-layouts/cms-build/app.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const app = express();
+
+app.('/',(req, res)=>{
+    
+})
+
+app.listen(3000,()=>{
+    console.log(`server running`)
+})
