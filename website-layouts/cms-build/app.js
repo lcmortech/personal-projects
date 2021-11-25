c96e15a43359560bece83059f400fc768d50d5be
Lauren <rg326@live.com>
2021-11-24 19:03:11 -0500

    added routes

--- website-layouts/cms-build/app.js
+++ website-layouts/cms-build/app.js
@@ -1,12 +1,28 @@
 //Module Import
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
+
+//Configure Mongoose to Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/cms-build',{userNewUrlParser: true})
+    .then(res => {
+    console.log('MongoDB connected successfully')
+    .catch(
+        console.log('Database connect failed')
+    )
+})
 
 //Configure express
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Routes
+app.use('/',(res, req)=>{
+    res.send('Welcome to the CMS App');
+})
 
 app.listen(3000,()=>{
     console.log(`server running`)
