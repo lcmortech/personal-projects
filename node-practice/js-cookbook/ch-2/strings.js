9a7170b7248759fe216cdaf389e438783d562efc
LC Morgan <rg326@live.com>
2022-01-07 18:17:24 -0500

    added notes on conditional string checks

--- node-practice/js-cookbook/ch-2/strings.js
+++ node-practice/js-cookbook/ch-2/strings.js
@@ -2,19 +2,28 @@
 
 let unknownVar = '9';
 if (typeof  unknownVar === 'string'){
-    console.log('true')
-}
-else{
+    console.log('true');
+} else {
 console.log('false');
 }
 
 //If you want to make sure that have a nonempty string
 if (typeof  unknownVar === 'string' && unknownVar.length > 0){
-    console.log('true')
-}
-else{
+    console.log('true');
+} else {
 console.log('false');
 }
 
 //If you want to check for blank strings (whitespace):
-if
+if(typeof unknownVar === 'string' && unknownVar.trim().length > 0){
+    console.log('true');
+} else {
+    console.log('false');
+}
+
+//If you either have a string primitive or an object that has the same prototype as string:
+if (typeof unknownVar === 'string' || String.prototype.isPrototypeOf(unknownVar)){ 
+    console.log('true');
+} else {
+    console.log('false');
+}
